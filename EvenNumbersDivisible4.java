d9912ccf9f6d5e55ba4a03ef58a0746a8060ede2
iremcelikk <102186287+iremcelikk@users.noreply.github.com>
2022-09-04 22:40:54 +0300

    Update EvenNumbersDivisible4.java

--- EvenNumbersDivisible4.java
+++ EvenNumbersDivisible4.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
+
 public class EvenNumbersDivisible4 {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int number=0, total=0;
-		
-		while(number%2==0) {
+		Scanner sc = new Scanner(System.in);
+		int number = 0, total = 0;
+
+		while (number % 2 == 0) {
 			System.out.println("Enter a number: ");
-			number=sc.nextInt();
-			if(number%4==0)
-				total+=number;
+			number = sc.nextInt();
+			if (number % 4 == 0)
+				total += number;
 		}
-		System.out.println("Sum of even numbers divisible by 4: "+total);
-
+		System.out.println("Sum of even numbers divisible by 4: " + total);
 	}
-
 }
